488aeb4b510e33a2654d3932237e48cafd650921
madezainul <madezainul@gmail.com>
2025-03-23 08:54:47 +0300

    Masalah render data di page report_detail

--- app/routes/Report.js
+++ app/routes/Report.js
@@ -1,20 +1,44 @@
 'use strict'
 
 const express = require('express'),
-      router = express.Router()
+router = express.Router();
+const { ReportDetails } = require('../models/ReportDetails');
 
-router.get('/report_add', async (req, res) => {
-    let context = { 
-        title: 'Tambah Laporan',
-    };
-    res.render('report/report_add', context);
+
+// Routes untuk handle report_add
+router.post('/report_add', async (req, res) => {
+    ReportDetails.add(req.body, (err) => {
+        if (err) {
+            return res.status(500).send('Error adding report');
+        }
+        res.redirect('/report_detail');
+    });
 });
 
+// Routes untuk handle report_detail
+// router.get('/report_detail', async (req, res) => {
+//     ReportDetails.all((err, rows) => {
+//         if (err) {
+//             return res.status(500).send('Error getting report');
+//         }
+//         // res.render('report_detail', { reports: rows });
+//         let context = {
+//             title: 'Report Detail',
+//             reports: rows
+//         };
+//         res.render('report_detail', context);
+//     });
+// });
+
+//Debugging
 router.get('/report_detail', async (req, res) => {
-    let context = { 
-        title: 'Detail Laporan',
-    };
-    res.render('report/report_detail', context);
+    ReportDetails.all((err, rows) => {
+        if (err) {
+            return res.status(500).send('Error getting report');
+        }
+        console.log(rows);
+        res.render('report_detail', { reports: rows });
+    });
 });
 
 module.exports = router;
